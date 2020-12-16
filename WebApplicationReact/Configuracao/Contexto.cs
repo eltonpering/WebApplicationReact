9371aabc902dfcd6a92619beef33604accd3f939
Elton A Pering <elton_pering@adsomos.com>
2020-12-16 14:10:16 -0300

    Add project files.

---
+++ WebApplicationReact/Configuracao/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationReact.Model;
+
+namespace WebApplicationReact.Configuracao
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ WebApplicationReact/Model/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationReact.Model
+{
+
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Descricao")]
+        public string Descricao { get; set; }
+    }
+}
